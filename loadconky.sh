1ea00ce643e2569fdbb628ba72b0b594534c92b9
= <reg-github@agt24.com>
2013-12-14 20:10:06 +0100

    Some changes for Gnome 3.10

--- loadconky.sh
+++ loadconky.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
+
 # RSS News Conky by https://github.com/tr37ion
 
 if [ "$(pidof conky)" ]; then
     killall conky
     sleep 5 
-    conky -c ~/.conky/global &
-    conky -c ~/.conky/rss_col1 &
-    conky -c ~/.conky/rss_col2 &
-    conky -c ~/.conky/rss_col3 &
-    conky -c ~/.conky/rss_col4 &
-#    conky -c ~/.conky/rss_arch &
+    conky -u 1600 -c ~/.conky/global &
+    conky -u 1600 -c ~/.conky/rss_col1 &
+    conky -u 1600 -c ~/.conky/rss_col2 &
+    conky -u 1600 -c ~/.conky/rss_col3 &
+    conky -u 1600 -c ~/.conky/rss_col4 &
     exit
 else
     sleep 5 
-    conky -c ~/.conky/global &
-    conky -c ~/.conky/rss_col1 &
-    conky -c ~/.conky/rss_col2 &
-    conky -c ~/.conky/rss_col3 &
-    conky -c ~/.conky/rss_col4 &
-#    conky -c ~/.conky/rss_arch &
+    conky -u 1600 -c ~/.conky/global &
+    conky -u 1600 -c ~/.conky/rss_col1 &
+    conky -u 1600 -c ~/.conky/rss_col2 &
+    conky -u 1600 -c ~/.conky/rss_col3 &
+    conky -u 1600 -c ~/.conky/rss_col4 &
     exit
 fi
