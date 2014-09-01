b533f6c780db47eeb58a47e21f48556b91c15b04
Pedro <pedro.proenca@fixeads.com>
2014-09-01 16:29:29 +0100

    Update loadconky.sh
    makes the file smaller

--- loadconky.sh
+++ loadconky.sh
@@ -1,22 +1,14 @@
 #!/bin/sh
 
 # RSS News Conky by https://github.com/tr37ion
-
 if [ "$(pidof conky)" ]; then
     killall conky
-    sleep 5 
-    conky -u 1600 -c ~/.conky/global &
-    conky -u 1600 -c ~/.conky/rss_col1 &
-    conky -u 1600 -c ~/.conky/rss_col2 &
-    conky -u 1600 -c ~/.conky/rss_col3 &
-    conky -u 1600 -c ~/.conky/rss_col4 &
-    exit
-else
-    sleep 5 
-    conky -u 1600 -c ~/.conky/global &
-    conky -u 1600 -c ~/.conky/rss_col1 &
-    conky -u 1600 -c ~/.conky/rss_col2 &
-    conky -u 1600 -c ~/.conky/rss_col3 &
-    conky -u 1600 -c ~/.conky/rss_col4 &
-    exit
 fi
+
+sleep 5 
+conky -u 1600 -c ~/.conky/global &
+conky -u 1600 -c ~/.conky/rss_col1 &
+conky -u 1600 -c ~/.conky/rss_col2 &
+conky -u 1600 -c ~/.conky/rss_col3 &
+conky -u 1600 -c ~/.conky/rss_col4 &
+exit
